d3f2e11bd96e474b8fd9a74b5f8211ee4d28f7bf
Dallas James <dallasjames42@gmail.com>
2019-10-11 17:34:45 -0600

    class component displaying players added i think for some reason I cant see the site

---
+++ client/src/classFetchPlayers.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class APICall extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            players: []
+        };
+    }
+
+    componentDidMount() {
+        axios.get(`https://http://localhost:5000/api/players`)
+            .then(res => this.setState({players: players}))
+            .catch(err => console.log(err))
+    }
+
+    render() {
+        const {players} = this.state;
+        console.log(players)
+
+        return (
+            <div>
+                {players.map(player => 
+                    <div key={player.id}>
+                        <h2>{player.name}</h2>
+                        <h3>Country: {player.country}</h3>
+                        <h3>Serches: {player.searches}</h3>
+                    </div>
+                    )}
+            </div>
+        )
+    }
+}
